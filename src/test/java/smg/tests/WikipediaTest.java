0257c3f14b36372a7e7a235022eeaf42dec17499
vunn92 <vunn30992@gmail.com>
2023-05-20 23:58:59 +0700

    edit Todos notes

--- src/test/java/smg/tests/WikipediaTest.java
+++ src/test/java/smg/tests/WikipediaTest.java
@@ -19,28 +19,18 @@ public class WikipediaTest extends BaseTest {
         wikipediaMainPage.open();
 
     }
-    /** Todo: Automating Test Case 26 in Test Design
-     *      Scenario: Verify search results of a search matching with a page name on Wikipedia
-     *      Given I access to https://en.wikipedia.org/wiki/Main_Page
-     *      When I fill <Software Testing> to "Search Wikipedia" text box
-     *      And I select "Search for pages containing Software Testing" option
-     *      Then I verify the search results on Search Results Page
-     */
 
-    /** Todo: Automating Test Case 27 in Test Design
-     *      Scenario: Verify search results of a search not matching with any page name on Wikipedia
-     *      Given I access https://en.wikipedia.org/wiki/Main_Page
-     *      When I fill <Software Testing 123> to "Search Wikipedia" text box
-     *      And I select "Search for pages containing Software Testing" option
-     *      Then I verify the search results on Search Results Page
-     * */
-
-    /** Todo: Automating Test Case 28 in Test Design
-     *      Scenario: Verify search results with no result found
-     *      Given I access https://en.wikipedia.org/wiki/Main_Page
-     *      When I fill <thisisfornoresultfoundsearch> to "Search Wikipedia" text box
-     *      And I select "Search for pages containing Software Testing" option
-     *      Then I verify the search results on Search Results Page
+    /** Todo: Automating Test Case 26, 27 and 28 in Test Design
+     *      Scenario: Verify search results
+     *          Given I access https://en.wikipedia.org/wiki/Main_Page
+     *          When I fill <searchText> to 'Search Wikipedia' text box
+     *          And I select 'Search for pages containing Software Testing' option
+     *          Then I verify the search results on Search Results Page
+     *              Examples:
+     *              |       Case           | searchText 				  |
+     *              | MatchingAPageName    | Software Testing 			  |
+     *              | NotMatchingAPageName | Software Testing 123		  |
+     *              | NoResultFound		   | thisisfornoresultfoundsearch |
      * */
     @DataProvider
     Object[][] searchTextData(){
