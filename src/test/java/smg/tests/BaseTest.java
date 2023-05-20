8c08621e9c1b31f10b0094c12da04b801fca1fa4
vunn92 <vunn30992@gmail.com>
2023-05-20 10:36:08 +0700

    change worklow for run selenium test

--- src/test/java/smg/tests/BaseTest.java
+++ src/test/java/smg/tests/BaseTest.java
@@ -5,11 +5,16 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import java.io.File;
 import java.io.IOException;
@@ -21,8 +26,9 @@ public class BaseTest {
     final int TIME_OUT_IN_SECONDS = 20;
 
     @BeforeClass(alwaysRun = true)
-    protected void setUp() {
-        driver = new ChromeDriver();
+    @Parameters({"browser"})
+    protected void setUp(String browser) {
+        driver = launchBrowser(browser);
         wait = new WebDriverWait(driver, Duration.ofSeconds(TIME_OUT_IN_SECONDS));
     }
 
@@ -46,6 +52,24 @@ public class BaseTest {
     protected void tearDown() {
         if (driver != null) {
             driver.quit();
+        }
+    }
+
+    private WebDriver launchBrowser(String name){
+        switch (name){
+            case "firefox": {
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.setHeadless(true);
+                return new FirefoxDriver(firefoxOptions);
+            }
+            case "safari":{
+                return new SafariDriver();
+            }
+            default: {
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.setHeadless(true);
+                return new ChromeDriver(chromeOptions);
+            }
         }
     }
 
